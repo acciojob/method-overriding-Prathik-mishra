a1b8c61f593b43886c34584f417392003b29c05f
Prathik Mishra <126965610+Prathik-mishra@users.noreply.github.com>
2024-03-12 16:07:49 +0530

    modified classes

---
+++ src/main/java/com/driver/A.java
@@ -0,0 +1,9 @@
+package com.driver;
+
+class A {
+  A() {
+  }
+    public String meth() {
+        return "Invoking method from class A";
+    }
+}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,20 +1,11 @@
 package com.driver;
-class A{
-  public String meth(){
-    return "Invoking method from class A";
-  } 
-}
-class B extends A{
-  public String meth(){
-    return "Method is overridden in Extended class B";
-  }
-}
+
 public class Main {
+  public Main() {
+  }
   public static void main(String[] args) {
     B obj = new B();
-
     obj.meth();
-
     obj.meth();
   }
 }
