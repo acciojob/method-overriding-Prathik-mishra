9f8bff2c64e2d431a93396b87ac3e91b275d3a79
Prathik Mishra <126965610+Prathik-mishra@users.noreply.github.com>
2024-03-12 15:59:38 +0530

    Added codes for method overriding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,18 +1,20 @@
 package com.driver;
 class A{
-  public string meth(){
+  public String meth(){
     return "Invoking method from class A";
   } 
 }
-class B extnds A{
-  public string meth(){
+class B extends A{
+  public String meth(){
     return "Method is overridden in Extended class B";
-  } 
+  }
 }
 public class Main {
-  B obj = new A();
-  obj.meth();
-  
-  B obj2 = new B();
-  obj2.meth();
+  public static void main(String[] args) {
+    B obj = new B();
+
+    obj.meth();
+
+    obj.meth();
+  }
 }
