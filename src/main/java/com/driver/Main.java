d782aed987e375147bca892c65e3873f42cc8f68
Prathik Mishra <126965610+Prathik-mishra@users.noreply.github.com>
2024-03-12 16:12:09 +0530

    modified

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,9 +3,9 @@ package com.driver;
 public class Main {
   public Main() {
   }
-  public static void main(String[] args) {
-    B obj = new B();
-    obj.meth();
-    obj.meth();
-  }
+    public static void main(String[] args) {
+      B obj = new B();
+      obj.meth();
+      obj.meth();
+    }
 }
