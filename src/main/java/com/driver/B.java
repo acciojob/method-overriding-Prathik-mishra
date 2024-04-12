e6b04cc8214ec4c00d740bdcc57e1fedfcc4794b
Prathik Mishra <126965610+Prathik-mishra@users.noreply.github.com>
2024-04-12 21:56:38 +0530

    added again

---
+++ src/main/java/com/driver/B.java
@@ -0,0 +1,11 @@
+package com.driver;
+
+public class B extends A {
+    B(){
+
+    }
+    @Override
+    public String meth(){
+        return "Method is overridden in Extended class B";
+    }
+}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,21 +1,5 @@
 package com.driver;
-public class A{
-    A(){
 
-    }
-    public String meth(){
-      return "Invoking method from class A";
-    }
-}
-public class B extends A {
-    B(){
-
-    }
-    @Override
-    public String meth(){
-      return "Method is overridden in Extended class B";
-    }
-}
 public class Main {
     Main(){
 
